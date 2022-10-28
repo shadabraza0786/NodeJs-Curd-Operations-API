460c96d5bd822246a3684f069e1a1dd1b46674b0
Shadab Raza <shadabraza500@gmail.com>
2022-10-29 02:15:41 +0530

    env add

--- src/db/connection.js
+++ src/db/connection.js
@@ -1,7 +1,19 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost:27017/crud-operations-api").then(() =>{
+mongoose.connect("mongodb+srv://shadabraza0786:<mongodb@123>@cluster0.gmr4imx.mongodb.net/?retryWrites=true&w=majority").then(() =>{
     console.log("Connection is Successfull......");
 }).catch((e) =>{
     console.log("No Connection", e);
 })
+
+
+
+
+
+// const mongoose = require("mongoose");
+
+// mongoose.connect("mongodb://localhost:27017/crud-operations-api").then(() =>{
+//     console.log("Connection is Successfull......");
+// }).catch((e) =>{
+//     console.log("No Connection", e);
+// })
