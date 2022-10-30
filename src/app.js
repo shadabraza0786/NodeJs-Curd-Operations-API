910ac15cafac19d6cd9c15bf9562f784fc74169a
Shadab Raza <shadabraza500@gmail.com>
2022-10-30 10:37:05 +0530

    Changes in enviroment

--- src/app.js
+++ src/app.js
@@ -12,53 +12,57 @@ const port = process.env.PORT || 3005
 
 //middlewares
 app.use(express.json());
-app.use(urlencoded({extended:true}));
+app.use(urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
     res.send("Hello I am live")
 });
 
-app.post("/addUsers", (req, res) => {
-    const user = new Users({
-        name : req.body.name,
-        username : req.body.username,
-        email : req.body.email,
-        phone : req.body.phone,
-        website : req.body.website
-    });
+// app.post("/addUsers", (req, res) => {
+//     const user = new Users({
+//         name : req.body.name,
+//         username : req.body.username,
+//         email : req.body.email,
+//         phone : req.body.phone,
+//         website : req.body.website
+//     });
 
-    user.save().then(()=>{
-        res.status(201).send(user)
-    }).catch((error) => {
-        res.status(400).send(error)
-    });
-});
-
-// app.post("/addUser", (req, res) => {
-
-//       console.log(req.body)
-//     const user = new Users(req.body);
-//     user.save().then(() =>{
+//     user.save().then(()=>{
 //         res.status(201).send(user)
 //     }).catch((error) => {
 //         res.status(400).send(error)
-//     })
-    
-// })
+//     });
+// });
 
-// app.get("/getUsers", (req, res) => {
-//     res.send("this is users data")
-// })
+app.post("/addUser", (req, res) => {
 
-app.get("/getUsers", async (req, res) => {
+    const user = new Users(req.body);
+    user.save().then(() => {
+        res.status(201).send(user)
+    }).catch((error) => {
+        res.status(400).send(error)
+    })
 
-    try{
-        const UsersData  = await Users.find();
-        res.send(UsersData)
-    }catch(error){
+})
+
+app.get("/getUsers", (req, res) => {
+    Users.find().then((users) => {
+        res.send(users)
+    }).catch((error) => {
         res.send(error)
-    }
-});
+    })
+})
+
+
+// app.get("/getUsers", async (req, res) => {
+
+//     try{
+//         const UsersData  = await Users.find();
+//         res.send(UsersData)
+//     }catch(error){
+//         res.send(error)
+//     }
+// });
 
 app.listen(port, () => {
     console.log(`connection is setup at ${port}`)
